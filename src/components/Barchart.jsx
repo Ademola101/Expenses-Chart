1a864158fa3674638dc1711a7ecd268226cbd07f
Ademola <ademolaogunmokun@yahoo.com>
2022-07-15 21:59:36 +0100

    lint applied

---
+++ src/components/Barchart.jsx
@@ -0,0 +1,11 @@
+
+
+const Barchart = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Barchart
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+const Header = ({ amount }) => {
+
+  return (
+    <header>
+
+      <div>
+        My balance
+      </div>
+      <div>
+        {amount}
+      </div>
+    </header> )
+}
+
+export default Header
+
