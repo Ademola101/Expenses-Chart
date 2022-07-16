a1714d47f3d6460e1369ba253b6693d53b3af95f
Ademola <ademolaogunmokun@yahoo.com>
2022-07-16 18:36:21 +0100

    complete

--- src/components/styles/FooterStyle.js
+++ src/components/styles/FooterStyle.js
@@ -4,6 +4,17 @@ export const FooterStyle = styled.footer`
 display: flex;
 margin-top: 30px;
 
+.last {
+  width: 100px;
+  margin-left: 100px;
+  font-size: small;
+  color: grey;
+}
+
+.percent {
+  margin-left: 150px;
+}
+
 
 .total {
   margin-right: 120px;
